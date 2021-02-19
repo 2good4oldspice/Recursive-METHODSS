160dceabdd4e283c52023812d217b39ef7a92452
Liam O'Neil <liam.o'neil@patelhs.org>
2021-02-19 15:42:27 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Welcome to Mcdonalds.");
+    System.out.println("May I take your order?.");
+    System.out.println("Yes, I would like a bigmac please.");
+    System.out.println("Would that be all?");
+    System.out.println("I would also like a 6 peice nugget please, and that will be all.");
+    double 
+  }
+
+  public static void saybigMac(int q) {
+    if (q == 0) {
+      System.out.println("Yes, I would like a bigmac please");
+  
+    }
+      
+  }
+
+
+
+
+}
